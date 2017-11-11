7076f72da0c9fc07c95d66f61fdf655ebe83151e
Adam Jetmalani <adamsinternetmailbox@gmail.com>
2017-11-11 16:02:25 -0500

    remove some cruft

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to n2t
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- test/n2t/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns n2t.core-test
-  (:require [clojure.test :refer :all]
-            [n2t.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
